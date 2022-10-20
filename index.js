3247a2c0f1ccf616b3f8c2aa5bb539b28d910ea2
Joey <joeyhightower@Joeys-MBP.bak.rr.com>
2022-10-20 08:10:42 -0700

    Starter

---
+++ index.js
@@ -0,0 +1,43 @@
+class Card{
+    constructor(rank, suit){ //52 cards
+        this.rank = rank; //13 ranks
+        this.suit = suit; //4 suits
+    }
+
+}
+
+class Hand{
+    constructor([cards]){
+        this.cards = [cards];
+    }
+
+    addCard(card){
+
+    }
+
+    playCard(card){
+
+    }
+}
+
+class Deck{
+    constructor([]){
+
+    }
+
+    generateCards(){
+
+    }
+
+    shuffleCards(){
+
+    }
+
+    draw(){
+
+    }
+
+    deal(numHands, cardsPerHand){
+
+    }
+}
